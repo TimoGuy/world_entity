ebe43f96855119d88a1857d10e41beb9284f145b
TimoGuy <timoguy99004@gmail.com>
2025-01-13 03:30:56 -0800

    Removes uint8 int dist template

--- src/guid.cpp
+++ src/guid.cpp
@@ -8,7 +8,6 @@ GUID GUID::generate()
     static thread_local std::mt19937 mt{ std::random_device{}() };
     static std::uniform_int_distribution<uint32_t> u32_dist;
     static std::uniform_int_distribution<uint16_t> u16_dist;
-    static std::uniform_int_distribution<uint8_t> u8_dist;
 
     GUID new_guid;
     new_guid.d1 = u32_dist(mt);
@@ -16,7 +15,8 @@ GUID GUID::generate()
     new_guid.d3 = u16_dist(mt);
     for (size_t i = 0; i < 8; i++)
     {
-        new_guid.d4[i] = u8_dist(mt);
+        // @NOTE: u8 uniform int distribution not supported, so fall back to u32.
+        new_guid.d4[i] = static_cast<uint8_t>(u32_dist(mt) % 256u);
     }
     return new_guid;
 }
